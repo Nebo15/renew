e958c973b58aec7cfad926c9704cdf94e83314b0
Andrew Dryga <andrew@dryga.com>
2017-03-19 15:55:07 +0200

    Suggest to purge debug error on compilation time

--- templates/mix/config/prod.exs
+++ templates/mix/config/prod.exs
@@ -2,4 +2,9 @@ use Mix.Config
 
 # Configuration for production environment.
 # It should read environment variables to follow 12 factor apps convention.
+
+# Sometimes you might want to improve production performance by stripping logger debug calls during compilation
+# config :logger,
+#   compile_time_purge_level: :info
+
 <%= @config_prod %>
