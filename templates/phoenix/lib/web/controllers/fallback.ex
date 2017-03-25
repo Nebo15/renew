dd872c2d2893b82ecc44589a18b1d9f9e4506126
Andrew Dryga <andrew@dryga.com>
2017-03-25 16:55:42 +0200

    Better phoenix fallback controller

--- templates/phoenix/lib/web/controllers/fallback.ex
+++ templates/phoenix/lib/web/controllers/fallback.ex
@@ -4,10 +4,28 @@ defmodule <%= @module_name %>.Web.Controllers.Fallback do
   """
   use PRM.Web, :controller
 
+  defp call(conn, {:error, :access_denied}) do
+    conn
+    |> put_status(:unauthorized)
+    |> render(EView.Views.Error, :"401")
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(EView.Views.Error, :"404")
+  end
+
   def call(conn, nil) do
     conn
     |> put_status(:not_found)
     |> render(EView.Views.Error, :"404")
+  end
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(EView.Views.ValidationError, :"422", changeset)
   end
 
   def call(conn, %Ecto.Changeset{valid?: false} = changeset) do
